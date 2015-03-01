42dede53313d0d3cf2a793298f303665d6a345f3
Eric Scott <EricScott@inside-162-236.wireless.utoronto.ca>
2015-03-01 15:18:16 -0500

    Update UserController.js, User.js
    Updated User’s Controller and Model to handle user registration.
    
    ex.
    
    1. http://localhost:1337/user/create?username=test&password=test1234
    2. JSON object will be displayed containing the provided username, as
    well as the hashed and salted password.

--- Phase2/infosec/api/controllers/UserController.js
+++ Phase2/infosec/api/controllers/UserController.js
@@ -6,6 +6,38 @@
  */
 
 module.exports = {
-	
-};
 
+	login: function (req, res) {
+		var bcrypt = require('bcrypt-nodejs');
+
+		User.findOneByUser(req.body.user).exec(function (err, user) {
+			if (err) {
+				res.json({ error: 'DB error' }, 500)
+			};
+
+			if (user) {
+				bcrypt.compare(req.body.password, user.password, function (err, match) {
+					if (err){
+						res.json({ error: 'Server error' }, 500);
+					}
+
+					// If the user/password provided is correct
+					if (match) {
+						req.session.user = user.id;
+						res.json(user);
+					}
+					// Otherwise, user/password provided is incorrect
+					else{
+						if (req.session.user) {
+							req.session.user = null;
+						}
+						res.json({ error: 'Invalid password' }, 400);
+					}
+				});
+			}
+			else {
+				res.json({ error: 'User not found' }, 404);
+			}
+		});
+	}
+};
