460904367d294e98e7fbcd79b9f94d62068fdbfc
Haris Shoaib <haris005@gmail.com>
2015-03-25 17:28:03 -0400

    AdminController changes

--- Phase3/infosec/api/controllers/AdminController.js
+++ Phase3/infosec/api/controllers/AdminController.js
@@ -60,6 +60,55 @@
     });
   },
 
+  // ACTION NAME: byTutorial
+  // USE: Action returns all the exercises which correspond to a given tutorial
+  // PARAMETERS/INPUTS:
+  //    id - ID of Tutorial
+  byTutorial: function(req, res){
+
+    // Get ID (Tutorial ID) parameter from HTTP Request
+    var tutorialId = req.param('id'); 
+
+    // Find all exercises in the Exercises Collection by Tutorial id
+    Exercise.find({ tutorialId: tutorialId }) 
+      .exec(function(err, exercises){
+
+        // If no error occurred, then return all the Exercises
+        if(!err){ 
+          return res.view('admin/exercises', { exercises: exercises });
+
+        // If error occurred, then return an empty array  
+        }else{ 
+          return res.view('admin/exercises', { exercises: [] });
+        }
+      });
+  },
+
+  // ACTION NAME: byId
+  // USE: Action returns exercise by a given ID
+  // PARAMETERS/INPUTS:
+  //    id - ID of Exercise
+  byTutorialId: function(req, res){
+
+    // Get ID (Tutorial ID) parameter from HTTP Request
+    var exerciseId = req.param('id'); 
+
+    // Find exercise by given ID
+    Exercise.findOne({ id: exerciseId }) 
+      .exec(function(err, exercise){
+
+        // If no error occurred, then return the exercise
+        if(!err){ 
+          return res.view('admin/exercise', { exercise: exercise });
+
+        // If error occurred or object not found then respond with 404  
+        }else{ 
+          console.log(err);
+          return res.send(404);
+        }
+      });
+  },
+
   /* Returns a list of categories to 'admin/createTutorial' view ,
    if successful */ 
   createTutorial: function(req, res) {
