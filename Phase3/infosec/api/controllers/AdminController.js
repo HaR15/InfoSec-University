2f2b4b4d70d4f759c23013410182675c26b9cf04
g3chowd <g3chowd@users.noreply.github.com>
2015-03-26 15:15:19 -0400

    added all category admin actions

--- Phase3/infosec/api/controllers/AdminController.js
+++ Phase3/infosec/api/controllers/AdminController.js
@@ -7,6 +7,9 @@
 
  module.exports = {
 
+  /*
+    Returns the admin dashboard view.
+  */
   dashboard: function (req, res) {
     Category.find().exec(function (err, categories) {
       if (err) {
@@ -33,107 +36,6 @@
       }
     });
   },
-
-   /* Returns the list of tutorials to 'admin/adminTutorials' view given a categoryId, 
-  and category object, if successful */ 
-  byCategory: function(req, res) {
-    var categoryId = req.param('id');
-    Tutorial.find({categoryId : categoryId}).exec(function(err, tutorials) {
-      if (!err && tutorials.length > 0) {
-        Category.findOne({id: categoryId}).exec(function(err, cat) {
-          if(!err && cat) {
-            return res.view('admin/adminTutorialsList', {
-              tutorials: tutorials,
-              category: cat
-            });
-          }
-        });
-      }else{                
-        return res.view('admin/adminTutorialsList', {
-          tutorials: [],
-          category: null
-        });
-      }
-    });
-  },
-
-  /* Returns a tutorial object to 'tutorial/tutorial' view given a tutorialId,
-   if successful */ 
-  byId: function(req, res) {
-    var tutorialId = req.param('id');
-    Tutorial.find({id : tutorialId}).exec(function(err, tutorials) {
-      if (!err) {
-        if (tutorials.length > 0) {
-          return res.view('admin/adminTutorial', { tutorial: tutorials[0] });
-        } else {
-          return res.view('admin/adminTutorial', { tutorial: []})
-        }
-
-      }
-    });
-  },
-
-  // ACTION NAME: byTutorial
-  // USE: Action returns all the exercises which correspond to a given tutorial
-  // PARAMETERS/INPUTS:
-  //    id - ID of Tutorial
-  byTutorial: function(req, res){
-
-    // Get ID (Tutorial ID) parameter from HTTP Request
-    var tutorialId = req.param('id'); 
-
-    // Find all exercises in the Exercises Collection by Tutorial id
-    Exercise.find({ tutorialId: tutorialId }) 
-      .exec(function(err, exercises){
-
-        // If no error occurred, then return all the Exercises
-        if(!err){ 
-          return res.view('admin/adminExercisesList', { exercises: exercises });
-
-        // If error occurred, then return an empty array  
-        }else{ 
-          return res.view('admin/adminExercisesList', { exercises: [] });
-        }
-      });
-  },
-
-  // ACTION NAME: byId
-  // USE: Action returns exercise by a given ID
-  // PARAMETERS/INPUTS:
-  //    id - ID of Exercise
-  byExerciseId: function(req, res){
-
-    // Get ID (Tutorial ID) parameter from HTTP Request
-    var exerciseId = req.param('id'); 
-
-    // Find exercise by given ID
-    Exercise.findOne({ id: exerciseId }) 
-      .exec(function(err, exercise){
-
-        // If no error occurred, then return the exercise
-        if(!err){ 
-          return res.view('admin/exercise', { exercise: exercise });
-
-        // If error occurred or object not found then respond with 404  
-        }else{ 
-          console.log(err);
-          return res.send(404);
-        }
-      });
-  },
-
-  /* Returns a list of categories to 'admin/createTutorial' view ,
-   if successful */ 
-  createTutorial: function(req, res) {
-    Category.find().exec(function(err, categories){
-      if (!err) {
-        return res.view('admin/adminTutorialsList', { categories: categories });
-      } else {
-        return res.view('500');
-      }
-    });
-  },
-
 
   /*
     DESCRIPTION
@@ -171,6 +73,66 @@
       return res.view("admin/categoryEditor", locals);
     }
   },
+
+  /*
+    DESCRIPTION
+      Saves an category to the database. If a category `id` parameter is 
+      supplied, then the changes to the respective category are saved, otherwise 
+      a new category is created. If an bad id is supplied, nothing happens.
+
+    PARAMETERS
+      id:   (optional) category id
+  */
+  saveCategory: function (req, res) {
+    var params = req.allParams();
+    var categoryEdit = {
+      title: params.title,
+      brief: params.brief,
+      description: params.description,
+    };
+
+    var saveAction;
+    if (params.id) { // update exercise if an exercise id was given
+      saveAction = Category.update({id:params.id}, categoryEdit);
+    
+    } else { // save a newly created exercise
+      saveAction = Category.create(categoryEdit);
+    }
+
+    // return respective page
+    saveAction.exec(function (err, category) {
+      if (err) { // error occurred
+        return res.serverError(JSON.stringify(err));
+      } else {
+        FlashService.success(req, 'Saved category!');
+        return res.redirect('/admin');  
+      }
+    });
+  },
+
+  /*
+    DESCRIPTION
+      Deletes the specified category.
+
+    PARAMENTERS
+      id:   (required) category id
+  */
+  deleteCategory: function (req, res) {
+    var categoryId = req.param('id');
+    Category.destroy({id:categoryId}).exec(function (err, deleted) {
+      if (err) {
+        return res.serverError(JSON.stringify(err));
+
+      } else if (deleted.length) {
+        FlashService.success(req, 'Successfully deleted category!');
+        return res.redirect('/admin');
+
+      } else {
+        return res.view('404');
+      }
+    });
+  }
+}
 
 
   /*
@@ -255,7 +217,7 @@
       if (err) { // error occurred
         return res.serverError(JSON.stringify(err));
       } else {
-        FlashService.success(req, 'Saved selected exercise!');
+        FlashService.success(req, 'Saved exercise!');
         return res.redirect('/admin');  
       }
     });
